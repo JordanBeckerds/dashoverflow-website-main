8427d3c0da54ab6f502fe6b8925d7afd66415c43
Jordan Becker <134543576+JordanBeckerds@users.noreply.github.com>
2025-01-06 14:02:51 +0100

    Add files via upload

---
+++ creations/res/js/header.js
@@ -0,0 +1,29 @@
+window.onscroll = function() {
+    var pos = window.pageYOffset;
+    if (pos > 200) {
+        document.querySelector('#main-header').style.top = '0';
+        document.querySelector('.header-stack').style.width = '100%'
+        document.querySelector('.header-stack').style.borderRadius = '0'
+        document.querySelector('.header-stack').style.transition = '0.6s';
+    } else {
+        document.querySelector('#main-header').style.top = '3vh';
+        document.querySelector('.header-stack').style.width = '80%'
+        document.querySelector('.header-stack').style.borderRadius = '1vw'
+        document.querySelector('.header-stack').style.transition = '0.6s';
+    }
+}
+function toggleAudio() {
+    var audioElement = document.getElementById('player')
+    var soundOn = document.getElementById('play')
+    var soundOff = document.getElementById('pause')
+    if (audioElement.paused) {
+      audioElement.play();
+      audioElement.volume = 0.1;
+      $(soundOn).show();
+      $(soundOff).hide();
+    } else {
+      audioElement.pause();
+      $(soundOn).hide();
+      $(soundOff).show();
+    }
+  } 
